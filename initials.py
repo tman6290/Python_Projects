a2d49618e7ebc465932bbe67ad73a168b0dd2174
Tofarati Farinu <62319828+tman6290@users.noreply.github.com>
2021-03-11 18:29:29 +0100

    Add files via upload
    A little project where I play around with the print statements and concatenation.

---
+++ initials.py
@@ -0,0 +1,12 @@
+# This code will print out my initilas in the form of ASCCII art
+
+# My name is Tofa Farinu
+# Fun fact about me: I love drawing and watching anime :)
+
+print("T T T T T"+"  F F F F F")
+print("    T    "+"  F       ")
+print("    T    "+"  F       ")
+print("    T    "+"  F F F     ")
+print("    T    "+"  F       ")
+print("    T    "+"  F       ")
+print("    T    "+"."+" F  ")
