beb4adba85a6e695939cc009cb50e2f9108f4d5c
Tofarati Farinu <62319828+tman6290@users.noreply.github.com>
2021-03-12 22:01:05 +0100

    Update initials.py

--- initials.py
+++ initials.py
@@ -3,10 +3,10 @@
 # My name is Tofa Farinu
 # Fun fact about me: I love drawing and watching anime :)
 
-print("T T T T T"+"  F F F F F")
-print("    T    "+"  F       ")
-print("    T    "+"  F       ")
-print("    T    "+"  F F F     ")
-print("    T    "+"  F       ")
-print("    T    "+"  F       ")
-print("    T    "+"."+" F  ")
+print("T  T  T  T  T"+"   F   F   F   F   F")
+print("     T    "+"      F       ")
+print("     T    "+"      F       ")
+print("     T    "+"      F   F   F     ")
+print("     T    "+"      F       ")
+print("     T    "+"      F       ")
+print("     T    "+"."+"     F  ")
